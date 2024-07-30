4374c3cfe1589580bbe87f501363609f4186bb51
Thanathip Limna <boatkrap@gmail.com>
2024-07-30 11:03:48 +0700

    add crud

--- digimon/main.py
+++ digimon/main.py
@@ -5,11 +5,26 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
+class BaseItem(BaseModel):
     name: str
     description: Optional[str] = None
     price: float = 0.12
     tax: Optional[float] = None
+
+
+class CreatedItem(BaseItem):
+    pass
+
+
+class Item(BaseItem):
+    id: int
+
+
+class ItemList(BaseModel):
+    items: list[Item]
+    page: int
+    page_size: int
+    size_per_page: int
 
 
 app = FastAPI()
@@ -20,6 +35,29 @@ def root():
     return {"message": "Hello World"}
 
 
+@app.post("/items")
+async def created_item(item: CreatedItem) -> Item:
+    print("created_item", item)
+    data = item.dict()
+    data["id"] = 1
+    return Item.parse_obj(data)
+
+
+@app.get("/items")
+async def read_items() -> ItemList:
+    return ItemList()
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int) -> Item:
-    return {"item_id": item_id}
+    return Item()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int) -> Item:
+    return Item()
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int) -> Item:
+    return Item()
